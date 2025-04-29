df1b224b500d7c886148afb3adc7cb11e711ff62
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-04-29 21:00:24 +0530

    add some code

---
+++ switch.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    switch(2){
+        case 1: cout<<"1";
+                break;
+        case 2:cout<<"2";
+                break;
+        case 3: cout<<"3";
+                break;
+        default:cout<<">=4";
+    }
+    
+    return 0;
+}
