9f8e7ce69a6749dff81647005c5a9d3ea83310e0
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-05-09 22:25:10 +0530

    input done

---
+++ hash.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<map>
+using namespace  std;
+int main(){
+    map<int,int> mp;
+    cout<<"Enter values to be entered into map:";
+    cout<<endl<<"Note: give 0 as a input to print the map";
+    int ele;
+    while(1){
+        if(ele==0)
+            break;
+        else{
+            mp[ele]++;
+            cout<<"element stored and updated it cnt";
+        }
+    }
+}
