b30c24dcd3fe8326dc2a3c3ecf4b404a5c56b300
kosanaharshavardhan <kosana.harshavardhan0705@gmail.com>
2025-05-09 22:29:36 +0530

    print code done

--- hash.cpp
+++ hash.cpp
@@ -4,7 +4,8 @@ using namespace  std;
 int main(){
     map<int,int> mp;
     cout<<"Enter values to be entered into map:";
-    cout<<endl<<"Note: give 0 as a input to print the map";
+    cout<<endl<<"Note: give 0 as a input to print the map:";
+    cout<<endl;
     int ele;
     while(1){
         cin>>ele;
@@ -14,5 +15,10 @@ int main(){
             mp[ele]++;
             cout<<"element stored and updated it cnt";
         }
+    }
+    cout<<"Here Goes the map:"<<"\n";
+        cout<<"Element"<<"->"<<"frequency";
+    for(auto i=mp.begin();i!=mp.end();i++){
+        cout<<i->first<<"->"<<i->second<<endl;
     }
 }
